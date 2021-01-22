4a18d9b3c93ccdb36847b08fa3d0ff12a786c9a3
AymanAlshaikh <ayman159@live.com>
2021-01-22 11:59:28 +0300

    DOne

--- src/Components/SinglePoints.js
+++ src/Components/SinglePoints.js
@@ -3,7 +3,7 @@ function SinglePoints(props) {
   return (
     <div>
       <h4>{props.Points.year}</h4>
-      <img src={props.Points.pic} />
+      <img src={props.Points.pic} alt="Pictures" />
       <p>{props.Points.describtion}</p>
     </div>
   );
